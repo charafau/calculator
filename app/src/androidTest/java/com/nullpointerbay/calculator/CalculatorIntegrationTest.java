93092b2b7d36abef1f648242db3bebf3356af480
charafau <gorudonu@gmail.com>
2017-04-15 05:45:37 +0900

    Integrated rpn calculator with main activity Added espresso tests

---
+++ app/src/androidTest/java/com/nullpointerbay/calculator/CalculatorIntegrationTest.java
@@ -0,0 +1,178 @@
+package com.nullpointerbay.calculator;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@RunWith(AndroidJUnit4.class)
+public class CalculatorIntegrationTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+
+
+    @Test
+    public void shouldAddSevenAndThree() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btnSeven), withText("7"), isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.btnAdd), withText("+"), isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.btwThree), withText("3"), isDisplayed()));
+        appCompatButton3.perform(click());
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(R.id.btnEqual), withText("="), isDisplayed()));
+        appCompatButton4.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.txtSummary), withText("10.0"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.instanceOf(android.widget.TableLayout.class),
+                                        1),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("10.0")));
+
+    }
+
+    @Test
+    public void shouldSubtractThreeFromEight() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btnEight), withText("8"), isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.btnMinus), withText("-"), isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.btwThree), withText("3"), isDisplayed()));
+        appCompatButton3.perform(click());
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(R.id.btnEqual), withText("="), isDisplayed()));
+        appCompatButton4.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.txtSummary), withText("5.0"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.instanceOf(android.widget.TableLayout.class),
+                                        1),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("5.0")));
+
+    }
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btnFive), withText("5"), isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.btnAdd), withText("+"), isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.btwThree), withText("3"), isDisplayed()));
+        appCompatButton3.perform(click());
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(R.id.btnEqual), withText("="), isDisplayed()));
+        appCompatButton4.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.txtSummary), withText("8.0"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.instanceOf(android.widget.TableLayout.class),
+                                        1),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("8.0")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.txtOutput), withText("5 + 3"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.instanceOf(android.widget.TableLayout.class),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView2.check(matches(withText("5 + 3")));
+
+        ViewInteraction appCompatButton5 = onView(
+                allOf(withId(R.id.btnClear), withText("C"), isDisplayed()));
+        appCompatButton5.perform(click());
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.txtOutput),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.instanceOf(android.widget.TableLayout.class),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView3.check(matches(withText("")));
+
+        ViewInteraction textView4 = onView(
+                allOf(withId(R.id.txtSummary),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.instanceOf(android.widget.TableLayout.class),
+                                        1),
+                                0),
+                        isDisplayed()));
+        textView4.check(matches(withText("")));
+
+    }
+
+
+}
--- app/src/main/java/com/nullpointerbay/calculator/MainActivity.java
+++ app/src/main/java/com/nullpointerbay/calculator/MainActivity.java
@@ -4,6 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +18,85 @@ public class MainActivity extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
     }
 
+    @BindView(R.id.txtOutput)
+    TextView txtOutput;
+    @BindView(R.id.txtSummary)
+    TextView txtSummary;
+    private RpnCalc rpnCalc;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        rpnCalc = new RpnCalc();
 
+    }
+
+    @OnClick({R.id.btnSeven, R.id.btnEight, R.id.btnNine, R.id.btnMinus, R.id.bntFour,
+            R.id.btnFive, R.id.btnSix, R.id.btnAdd, R.id.btnOne, R.id.btnTwo, R.id.btwThree,
+            R.id.buttonDot, R.id.btnZero, R.id.btnEqual, R.id.btnClear})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btnSeven:
+                appendStringToOutput(7);
+                break;
+            case R.id.btnEight:
+                appendStringToOutput(8);
+                break;
+            case R.id.btnNine:
+                appendStringToOutput(9);
+                break;
+            case R.id.btnMinus:
+                appendOperatorToOutput(" - ");
+                break;
+            case R.id.bntFour:
+                appendStringToOutput(4);
+                break;
+            case R.id.btnFive:
+                appendStringToOutput(5);
+                break;
+            case R.id.btnSix:
+                appendStringToOutput(6);
+                break;
+            case R.id.btnAdd:
+                appendOperatorToOutput(" + ");
+                break;
+            case R.id.btnOne:
+                appendStringToOutput(1);
+                break;
+            case R.id.btnTwo:
+                appendStringToOutput(2);
+                break;
+            case R.id.btwThree:
+                appendStringToOutput(3);
+                break;
+            case R.id.buttonDot:
+                appendOperatorToOutput(".");
+                break;
+            case R.id.btnZero:
+                appendStringToOutput(0);
+                break;
+            case R.id.btnEqual:
+                final CharSequence text = txtOutput.getText();
+                if (!TextUtils.isEmpty(text)) {
+                    final String postfixEquation = rpnCalc.transformInfixToPostfix(text.toString());
+                    final double result = rpnCalc.calculate(postfixEquation);
+                    this.txtSummary.setText(String.valueOf(result));
+                }
+                break;
+            case R.id.btnClear:
+                this.txtOutput.setText("");
+                this.txtSummary.setText("");
+                break;
+        }
+    }
+
+    private void appendStringToOutput(int seven) {
+        this.txtOutput.setText(String.format("%s%d", this.txtOutput.getText(), seven));
+    }
+
+    private void appendOperatorToOutput(String operator) {
+        this.txtOutput.setText(String.format("%s%s", this.txtOutput.getText(), operator));
     }
 }
